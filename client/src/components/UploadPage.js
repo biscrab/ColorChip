0a735a73e5d1531545add896d029521af57810d0
biscrab <biscrab@naver.com>
2021-11-07 18:27:55 +0900

    color

--- client/src/components/UploadPage.js
+++ client/src/components/UploadPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as S from '../styled/App'
 import axios from "axios";
 
@@ -6,35 +6,37 @@ const UploadPage = () => {
 
     const [list, setList] = useState([])
     const [name, setName] = useState();
-    const [input, setInput] = useState();
+    const [input, setInput] = useState('#');
+
+    useEffect(()=>{
+        if(input === ''){
+            setInput('#');
+        }
+    },[input])
 
     const onChange = (value) => {
-        setInput(value.replace(/[^a-zA-Z0-9]/ig, ''));
+        if(input.length <= 7){
+            setInput(value.replace(/[^a-f0-9#]/ig, ''));
+        }
     }
 
     const addColor = () => {
+        console.log(input);
+        if(list.length <= 100 && input.length >= 3){
         if(input){
-            if(list.includes(item => item = input)){
+            if(list.includes(input)){
                 alert("중복되는 색 입니다.")
             }
             else{
-                if(input[0] === "#"){
-                    if(4 <= input.length&&input.length <= 7){
-                    setList([...list, input]);
-                    setInput("");
-                }
+                console.log(list.includes(item => item === input))
+                setList([...list, input]);
             }
-            else{
-                if(3 <= input.length&&input.length <= 6){
-                    setList([...list, "#" + input]);
-                    setInput("");
-                    }
-                }
             }
         }
         else{
-            alert("입력을 해주세요")
+            alert("잘못된 입력입니다.")
         }
+        setInput("#");
     }
 
     const Regist = () => {
@@ -59,10 +61,10 @@ const UploadPage = () => {
                 <h2>제목</h2>
                 <input placeholder="제목을 입력해주세요." onChange={(e)=>setName(e.target.value)} value={name}></input>
                 </S.PB>
-                <h2>색깔</h2>
+                <h2>색깔({list.length}/100)</h2>
                 <S.ACDiv>
                 <S.Input placeholder="색을 입력해주세요." onChange={(e)=>onChange(e.target.value)} value={input} />
-                <button onClick={()=>addColor()}>추가</button>
+                <button onClick={()=>addColor()}>추가{input}</button>
                 </S.ACDiv>
                 {list.map(
                     i => (
